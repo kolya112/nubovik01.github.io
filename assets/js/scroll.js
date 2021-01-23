05fbea253accf5d4c3c3ed1db3d4b045441d7bed
Nubovik <50026919+Nubovik01@users.noreply.github.com>
2021-01-24 02:01:14 +0300

    update!

---
+++ assets/js/scroll.js
@@ -0,0 +1,4 @@
+$('#container').perfectScrollbar();    // Initialize
+	$('#container').perfectScrollbar({});   // with options
+	$('#container').perfectScrollbar('update');  // Update
+	$('#container').perfectScrollbar('destroy'); // Destroy
